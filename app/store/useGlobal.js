458373708df9287786eab3302080c53ec83ef407
Rentoo <dreamYiZ@outlook.com>
2024-06-30 22:29:15 +0800

    -

--- app/store/useGlobal.js
+++ app/store/useGlobal.js
@@ -1,5 +1,8 @@
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
+const packageJson = require('../../package.json');
+
+
 
 const BG_TYPE = {
     IMAGE: 'image',
@@ -18,12 +21,14 @@ const useGlobalStore = create(persist(
             type: BG_TYPE.IMAGE
         },
         mode: MODE.EDIT,  // 默认为 'edit' 模式
+        version: process.env.NEXT_PUBLIC_VERSION,
         setMode: (_mode) => set(() => ({ mode: _mode })),  // 添加一个新的动作来更新 'mode' 状态
         setBoxArr: (_boxArr) => set((state) => ({ boxArr: _boxArr })),
         setModeToEdit: () => set(() => ({ mode: MODE.EDIT })),  // 快捷动作，将 'mode' 状态改为 'edit'
         setModeToDisplay: () => set(() => ({ mode: MODE.DISPLAY })),  // 快捷动作，将 'mode' 状态改为 'display'
         setModeToTest: () => set(() => ({ mode: MODE.TEST })),  // 快捷动作，将 'mode' 状态改为 'test'
         hideWhenDisplaying: () => get().mode !== MODE.DISPLAY,  // 当模式为 'display' 时返回 true，否则返回 false
+
     }),
 
     {
