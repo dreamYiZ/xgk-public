458ff684b5980f531840db4bdead79053a2a8587
Rentoo <dreamYiZ@outlook.com>
2024-07-01 09:05:59 +0800

    -

--- publish-build.sh
+++ publish-build.sh
@@ -2,4 +2,4 @@
 npm run build
 cp -r .next ../bibi-f-n/
 cp -r public ../bibi-f-n/
-find . -maxdepth 1 -type f ! -name "wrangler.toml" ! -name "*.bash" -exec cp {} ../bibi-f-n/ \;
+find . -maxdepth 1 -type f ! -name "wrangler.toml" ! -name "*.bash" ! -name "LICENSE" -exec cp {} ../bibi-f-n/ \;
