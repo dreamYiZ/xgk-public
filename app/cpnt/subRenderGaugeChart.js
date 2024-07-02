e97e9c66bfaae08f46adfd882194a3dad408323b
Rentoo <dreamYiZ@outlook.com>
2024-07-03 05:16:08 +0800

    -

--- app/cpnt/subRenderGaugeChart.js
+++ app/cpnt/subRenderGaugeChart.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { Gauge } from '@mui/x-charts/Gauge';
 import { BASIC_PAYLOAD_GAUGE_CHART } from "../util/util";
+import ChartMuiTheme from "./ChartMuiTheme";
 
 export default function SubRenderGaugeChart({ box, ...sub }) {
   return (
-    <Gauge
-      width={sub?.width || BASIC_PAYLOAD_GAUGE_CHART.width}
-      height={sub?.height || BASIC_PAYLOAD_GAUGE_CHART.height}
-      value={sub?.value || BASIC_PAYLOAD_GAUGE_CHART.value}
-    />
+    <ChartMuiTheme>
+      <Gauge
+        width={sub?.width || BASIC_PAYLOAD_GAUGE_CHART.width}
+        height={sub?.height || BASIC_PAYLOAD_GAUGE_CHART.height}
+        value={sub?.value || BASIC_PAYLOAD_GAUGE_CHART.value}
+      />
+    </ChartMuiTheme>
   );
 }
--- app/cpnt/subRenderLineChart.js
+++ app/cpnt/subRenderLineChart.js
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
 import { BASIC_PAYLOAD_LINE_CHART } from "../util/util";
+import ChartMuiTheme from "./ChartMuiTheme";
 
 export default function SubRenderLineChart({ box, ...sub }) {
   return (
-    <LineChart
-      xAxis={sub?.xAxis || BASIC_PAYLOAD_LINE_CHART.xAxis}
-      series={sub?.series || BASIC_PAYLOAD_LINE_CHART.series}
-      width={sub?.width || BASIC_PAYLOAD_LINE_CHART.width}
-      height={sub?.height || BASIC_PAYLOAD_LINE_CHART.height}
-    />
+    <ChartMuiTheme>
+
+      <LineChart
+        xAxis={sub?.xAxis || BASIC_PAYLOAD_LINE_CHART.xAxis}
+        series={sub?.series || BASIC_PAYLOAD_LINE_CHART.series}
+        width={sub?.width || BASIC_PAYLOAD_LINE_CHART.width}
+        height={sub?.height || BASIC_PAYLOAD_LINE_CHART.height}
+      />
+    </ChartMuiTheme>
+
   );
 }
