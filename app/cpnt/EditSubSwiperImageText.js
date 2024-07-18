7be3cf1fd2a05a73a210cdbafc00882d79c2b4c3
Rentoo <dreamYiZ@outlook.com>
2024-07-18 17:56:38 +0800

    swiper image text edit

--- app/cpnt/EditSubSwiperImageText.js
+++ app/cpnt/EditSubSwiperImageText.js
@@ -44,50 +44,43 @@ export default function () {
   const sub = useMemo(() => activeBox?.sub, [activeBox, activeBoxId]);
   const changeById = useBoxStore(state => state.changeById);
 
-
   const [isOpen, setIsOpen] = useState(false);
-
   const [option, setOption] = useState('');  // 新增的状态和处理函数
-
-
   const [tabIndex, setTabIndex] = useState(0);
-
 
   const [color, setColor] = useState();
   const [nameFontSize, setNameFontSize] = useState();
   const [descFontSize, setDescFontSize] = useState();
   const [commentFontSize, setCommentFontSize] = useState();
   const [timeDuration, setTimeDuration] = useState();
-  const [faceWidth, setFaceWidth] = useState();;
+  const [faceWidth, setFaceWidth] = useState();
   const [commentTime, setCommentTime] = useState();
   const [slidesPerView, setSlidesPerView] = useState();
-
-
-
-
+  const [fontSize, setFontSize] = useState();
+  const [lineHeight, setLineHeight] = useState();
+  const [textWidth, setTextWidth] = useState();
+  const [imageWidth, setImageWidth] = useState();
+  const [imageHeight, setImageHeight] = useState();
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
+  const [textMarginBottom, setTextMarginBottom] = useState();
 
   const saveChange = () => {
     if (sub) {
-
-
       let data = [];
-
       try {
-
         let _data = JSON.parse(option)
         if (Array.isArray(_data)) {
           data = _data;
         }
       } catch (e) {
         alert('json error: ' + e.message);
-        return
         console.log('error', e);
+        return;
       }
 
       let rest = {};
-      if (sub.type === SUB_TYPE.SWIPER_JS) {
-        rest.commentTime = commentTime;
-      }
+
 
       changeById(activeBox.boxid, {
         sub: {
@@ -95,53 +88,47 @@ export default function () {
           data,
           color: color,
           timeDuration: safeNumberIfString(timeDuration),
-          nameFontSize: safeNumberIfString(nameFontSize),
-          descFontSize: safeNumberIfString(descFontSize),
-          commentFontSize: safeNumberIfString(commentFontSize),
-          faceWidth: safeNumberIfString(faceWidth),
-          slidesPerView: safeNumberIfString(slidesPerView),
+          fontSize: safeNumberIfString(fontSize),
+          lineHeight: safeNumberIfString(lineHeight),
+          textWidth: safeNumberIfString(textWidth),
+          imageWidth: safeNumberIfString(imageWidth),
+          imageHeight: safeNumberIfString(imageHeight),
+          width: safeNumberIfString(width),
+          height: safeNumberIfString(height),
+          textMarginBottom: safeNumberIfString(textMarginBottom),
           ...rest
         },
       });
     }
   }
 
-
-
   useEffect(() => {
     if (activeBoxId && sub) {
-      const { tableHead, data } = sub;
-
+      const { data } = sub;
       setOption(JSON.stringify(data, null, 2));
-
-
-
-      setNameFontSize(sub.nameFontSize)
-      setDescFontSize(sub.descFontSize)
-      setCommentFontSize(sub.commentFontSize)
       setColor(sub.color);
-      setTimeDuration(sub.timeDuration);
-      setFaceWidth(sub.faceWidth);
-      setSlidesPerView(sub.slidesPerView);
+      setFontSize(sub.fontSize);
+      setLineHeight(sub.lineHeight);
+      setTextWidth(sub.textWidth);
+      setImageWidth(sub.imageWidth);
+      setImageHeight(sub.imageHeight);
+      setWidth(sub.width);
+      setHeight(sub.height);
+      setTextMarginBottom(sub.textMarginBottom);
 
 
-      if (sub.type === SUB_TYPE.SWIPER_JS) {
-        setCommentTime(sub.commentTime);
-      }
     }
   }, [sub, activeBoxId]);
 
-
   return (
     <Box my={2}>
-
-      <DrawerEditLayout saveChange={saveChange}
+      <DrawerEditLayout
+        saveChange={saveChange}
         isOpen={isOpen}
         setIsOpen={setIsOpen}
         buttonText="编辑数据"
-        title="轮播图"
+        title="轮播图-图文"
       >
-
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={tabIndex} onChange={(event, newValue) => setTabIndex(newValue)} aria-label="basic tabs example">
             <Tab label="数据" />
@@ -151,14 +138,11 @@ export default function () {
 
         <Box mt={1}></Box>
 
-
         <Box sx={{}} hidden={tabIndex !== 0}>
-
           <Typography variant="h6" component="h6">
             数据
           </Typography>
-
-          <Box >
+          <Box>
             <AceEditor
               mode="json"
               theme="monokai"
@@ -182,48 +166,35 @@ export default function () {
           </Box>
         </Box>
 
-        <Box sx={{}} hidden={tabIndex !== 1} >
-          <TextField value={faceWidth} onChange={(event) => setFaceWidth(event.target.value)} type="number" label="图片宽度" variant="outlined" />
-          <Box mt={1}></Box>
-
-          <TextField value={commentFontSize} onChange={(event) => setCommentFontSize(event.target.value)} type="number" label="图片高度" variant="outlined" />
-          <Box mt={1}></Box>
+        <Box sx={{}} hidden={tabIndex !== 1}>
+          <Typography variant="h6" component="h6">
+            样式
+          </Typography>
 
           <ColorField label="颜色" value={color} onChange={(event) => setColor(event)} />
           <Box mt={1}></Box>
 
-          <TextField value={nameFontSize} onChange={(event) => setNameFontSize(event.target.value)} type="number" label="行高" variant="outlined" />
           <Box mt={1}></Box>
-
-          <TextField value={descFontSize} onChange={(event) => setDescFontSize(event.target.value)} type="number" label="文字宽度" variant="outlined" />
-          <Box mt={1}></Box>
-
-          <TextField value={commentTime} onChange={(event) => setCommentTime(event.target.value)} type="number" label="文字间距" variant="outlined" />
-          <Box mt={1}></Box>
-          <Box mt={1}></Box>
-
-          <TextField value={slidesPerView} onChange={(event) => setSlidesPerView(event.target.value)} type="number" label="宽度" variant="outlined" />
-          <Box mt={1}></Box>
-
-          <TextField value={slidesPerView} onChange={(event) => setSlidesPerView(event.target.value)} type="number" label="高度" variant="outlined" />
-          <Box mt={1}></Box>
-
           <TextField value={timeDuration} onChange={(event) => setTimeDuration(event.target.value)} type="number" label="动画间隔" variant="outlined" />
           <Box mt={1}></Box>
-
-
-
-
-          {/* </Box> */}
+          <TextField value={fontSize} onChange={(event) => setFontSize(event.target.value)} type="number" label="字体大小" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={lineHeight} onChange={(event) => setLineHeight(event.target.value)} type="number" label="行高" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={textWidth} onChange={(event) => setTextWidth(event.target.value)} type="number" label="文字宽度" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={imageWidth} onChange={(event) => setImageWidth(event.target.value)} type="number" label="图片宽度" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={imageHeight} onChange={(event) => setImageHeight(event.target.value)} type="number" label="图片高度" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={width} onChange={(event) => setWidth(event.target.value)} type="number" label="宽度" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={height} onChange={(event) => setHeight(event.target.value)} type="number" label="高度" variant="outlined" />
+          <Box mt={1}></Box>
+          <TextField value={textMarginBottom} onChange={(event) => setTextMarginBottom(event.target.value)} type="number" label="文字底部间距" variant="outlined" />
+          <Box mt={1}></Box>
         </Box>
-
-
       </DrawerEditLayout>
-
     </Box>
   );
 }
-
-
-
-
