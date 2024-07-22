9c04a9bfc66b9d7132d8e3fd078a5f0aa83c6300
Rentoo <dreamYiZ@outlook.com>
2024-07-22 18:58:10 +0800

    -

--- app/cpnt/SubRenderMarquee.js
+++ app/cpnt/SubRenderMarquee.js
@@ -11,20 +11,20 @@ export default function (
   const { marqueeType, time, fontSize, color } = sub
 
   return <div style={{ width: box.width, height: box.height, }} >
-    <Box sx={{ overflow: "hidden", height: "100%" }}>
-      <Box sx={{ animation: `${classes.marqueeTop} ${time}s linear infinite` }}>
-        {marqueeType === MARQUEE_TYPE.BASIC && <RenderMarqueeBase sub={sub} />}
-      </Box>
+    <Box sx={{ overflow: "hidden", height: "100%", position: "relative" }}>
+      {marqueeType === MARQUEE_TYPE.BASIC && <RenderMarqueeBase sub={sub} />}
     </Box>
-  </div>
+  </div >
 }
 
 
 const RenderMarqueeBase = ({ sub }) => {
   const { data, color, fontSize } = sub;
-  return <Box >
-    {data.map(line => {
-      return <div style={{ color: color, fontSize: `${fontSize}px` }}>{line}</div>
-    })}
+  return <Box sx={{ animation: `${classes.marqueeTop} ${sub?.time}s linear infinite` }}>
+    <Box >
+      {data.map(line => {
+        return <div style={{ color: color, fontSize: `${fontSize}px` }}>{line}</div>
+      })}
+    </Box>
   </Box>
 }
