7703ff020fef3c7254d46f8b26dd93e6c4bf6e65
welive <dreamyiz@outlook.com>
2024-07-30 19:50:17 +0800

    -

--- app/util/util.js
+++ app/util/util.js
@@ -806,7 +806,24 @@ export const createBoxFabricCanvas = () => {
 
 
 export const BASIC_PAYLOAD_CHARTJS = {
-  payload: null,
+  payload: {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  },
 }
 
 export const createBoxChartjs = () => {
